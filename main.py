fe5647a68909ed2d66aa29f805db50c811db4193
Jan Pongratz <jan.pongratz@outlook.com>
2024-04-14 16:28:23 -0500

    Added trapezoidal sums; Changed compute to computation in README.md

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ function = input("Function to be integrated? (x should be the only variable)\n")
 a = float(input("Lower bound of integration?\n").replace(" ", "").replace("sqrt", "math.sqrt"))
 b = float(input("Upper bound of integration?\n").replace(" ", "").replace("sqrt", "math.sqrt"))
 p = int(input("Number of partitions?\n").replace(" ", ""))
-m = str(input("What method should be used? [L]RAM, [M]RAM, [R]RAM\n").replace(" ", "").replace("L", "l").replace("M", "m").replace("R", "r").replace("LRAM", "l").replace("MRAM", "m").replace("RRAM", "r").replace("lram", "l").replace("mram", "m").replace("rram", "r").replace("[L]RAM", "l").replace("[M]RAM", "m").replace("[R]RAM", "r").replace("[l]ram", "l").replace("[m]ram", "m").replace("[r]ram", "r"))
+m = str(input("What method should be used? [L]RAM, [M]RAM, [R]RAM, [T]rapezoidal\n").replace(" ", "").replace("L", "l").replace("M", "m").replace("R", "r").replace("LRAM", "l").replace("MRAM", "m").replace("RRAM", "r").replace("lram", "l").replace("mram", "m").replace("rram", "r").replace("[L]RAM", "l").replace("[M]RAM", "m").replace("[R]RAM", "r").replace("[l]ram", "l").replace("[m]ram", "m").replace("[r]ram", "r").replace("[L]", "l").replace("[M]", "m").replace("[R]", "r").replace("[l]", "l").replace("[m]", "m").replace("[r]", "r").replace("T", "t").replace("Trapezoidal", "t").replace("trapezoidal", "t").replace("[T]", "t").replace("[t]", "t"))
 delta_x = (b-a)/p
 
 def f(x):
@@ -24,6 +24,10 @@ def main():
     if m == "r":
         for n in range(p):
             temp = temp + ((delta_x*f(a+n*delta_x+delta_x)))
+            print("Iterations left: " + str(p-n))
+    if m == "t":
+        for n in range(p):
+            temp = temp + ((((delta_x*f(a+n*delta_x))))+(((delta_x*f(a+n*delta_x+delta_x)))))/(2)
             print("Iterations left: " + str(p-n))
     return temp
 
